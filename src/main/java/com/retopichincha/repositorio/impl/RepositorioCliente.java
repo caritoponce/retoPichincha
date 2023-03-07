7e31681f08420230e785a433660a9b7deba87875
GLOBANT\carolina.ponce <carolina.ponce@globant.com>
2023-03-06 22:32:24 -0500

    Se actualiza con el entitymanager

--- src/main/java/com/retopichincha/repositorio/impl/RepositorioCliente.java
+++ src/main/java/com/retopichincha/repositorio/impl/RepositorioCliente.java
@@ -1,5 +1,8 @@
 package com.retopichincha.repositorio.impl;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.stereotype.Repository;
 
 import com.retopichincha.entidades.Cliente;
@@ -7,6 +10,9 @@ import com.retopichincha.repositorio.ObjetoRepositorio;
 
 @Repository
 public class RepositorioCliente implements ObjetoRepositorio<Cliente> {
+	
+	@PersistenceContext
+	private EntityManager em;
 
 	@Override
 	public void guardar(Cliente t) {
--- src/main/java/com/retopichincha/repositorio/impl/RepositorioCuenta.java
+++ src/main/java/com/retopichincha/repositorio/impl/RepositorioCuenta.java
@@ -1,5 +1,8 @@
 package com.retopichincha.repositorio.impl;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.stereotype.Repository;
 import com.retopichincha.entidades.Cuenta;
 import com.retopichincha.repositorio.ObjetoRepositorio;
@@ -7,6 +10,9 @@ import com.retopichincha.repositorio.ObjetoRepositorio;
 @Repository
 public class RepositorioCuenta  implements ObjetoRepositorio<Cuenta> {
 
+	@PersistenceContext
+	private EntityManager em;
+	
 	@Override
 	public void guardar(Cuenta t) {
 		// TODO Auto-generated method stub
